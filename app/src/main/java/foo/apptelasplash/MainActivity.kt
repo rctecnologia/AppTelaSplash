d298dba10924c0450d0165074b72800cc216c9d6
rcosta1986 <ronaldo.suam@gmail.com>
2021-08-16 13:41:24 -0300

    Initial commit

---
+++ app/src/main/java/foo/apptelasplash/MainActivity.kt
@@ -0,0 +1,17 @@
+package foo.apptelasplash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun fecharApp(view: View) {
+
+        finish()
+    }
+}
